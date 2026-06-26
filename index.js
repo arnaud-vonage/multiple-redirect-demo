1add7fa9e31198b42fa2875052a79b3c1fd4669c
arnaud-vonage <arnaud.rachez@vonage.com>
2026-06-26 11:26:27 +0900

    Fix SSE: add X-Accel-Buffering: no to disable nginx proxy buffering

--- index.js
+++ index.js
@@ -258,6 +258,7 @@ app.get('/_/debug/live', async (req, res) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
+    res.setHeader('X-Accel-Buffering', 'no');
     res.flushHeaders();
 
     sseClients.add(res);
